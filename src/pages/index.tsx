57a9b3d37402516c19b5343d3736c75fe7d7b2a8
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-12 13:23:28 -0300

    creating mock design home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import Image from "next/future/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import c1 from "../assets/1.png";
+import c2 from "../assets/2.png";
+import c3 from "../assets/3.png";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Começou</h1>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={c1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={c2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      {/* <Product>
+        <Image src={c3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
   );
 }
