8da204150a1e74c3fe71c51fc2d31947e91968c8
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-13 17:05:22 -0300

    validating next api

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,9 +5,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
+  }
+
   const { priceId } = req.body;
 
-  console.log(priceId);
+  if (!priceId) {
+    return res.status(400).json({ error: "Price not found." });
+  }
 
   const checkoutSession = await stripe.checkout.sessions.create({
     cancel_url: `${process.env.NEXT_URL}/`,
